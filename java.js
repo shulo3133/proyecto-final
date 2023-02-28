25a70495bd01b37be70e1231450e87d3e0a0bd66
DESKTOP-0A2BIV1\Docto <lautaroelian313@hotmail.com>
2023-02-28 18:50:54 -0300

    trabajo integrador

---
+++ java.js
@@ -0,0 +1,10 @@
+document.addEventListener('click', function(){
+console.log("el mail es: emilyjohnston171@gmail.com ");
+document.getElementById("demo").innerHTML="emilyjohnston171@gmail.com"
+})
+
+const collection = document.getElementsByClassName(azul);
+for (let i = 0; i < collection.length; i++) {
+   collection[i].style.backgroundColor= "grey"
+}
+    
